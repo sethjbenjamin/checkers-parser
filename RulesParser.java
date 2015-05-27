af53925f83c6746cbc879672f3d693d48b86a6e6
Seth Benjamin <sethjbenjamin@gmail.com>
2015-05-27 17:47:38 -0400

    Searches for directional adverb modifying 'move', specifically forward and diagonal. Does not yet support morphological variation of 'move'.

--- RulesParser.java
+++ RulesParser.java
@@ -1,22 +1,23 @@
 import java.io.*;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Properties;
 
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
+//import edu.stanford.nlp.dcoref.CorefChain;
+//import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
 import edu.stanford.nlp.io.*;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.trees.*;
+//import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.util.*;
 
 public class RulesParser
 {
 	private String fileName;
 	private StanfordCoreNLP pipeline;
-	private Annotation annotatedRules;
+	private Annotation annotation;
 	private List<CoreMap> sentences;
 
 
@@ -34,10 +35,10 @@ public class RulesParser
 	{
 		try
 		{
-			annotatedRules = new Annotation(IOUtils.slurpFile(fileName));
-			pipeline.annotate(annotatedRules);
+			annotation = new Annotation(IOUtils.slurpFile(fileName));
+			pipeline.annotate(annotation);
 
-			sentences = annotatedRules.get(CoreAnnotations.SentencesAnnotation.class);
+			sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
 		}
 		catch (IOException e)
 		{
@@ -53,15 +54,16 @@ public class RulesParser
 		{
 			System.out.println(sentence.toShorterString());
 		}*/
-		System.out.println(sentences.get(8).toShorterString());
-
+		//System.out.println(sentences.get(8).toShorterString());
+		ArrayList<Direction> motionTypes = parseMotion();
 
 		
 	}
 
-	/*public Direction parseMotion(String s)
+	//TODO: EVENTUALLY make this return a Direction
+	public ArrayList<Direction> parseMotion()
 	{
-		for(CoreMap sentence: sentences) 
+		/*for(CoreMap sentence: sentences) 
 		{
 	      	// traversing the words in the current sentence
 	      	// a CoreLabel is a CoreMap with additional token-specific methods
@@ -72,9 +74,35 @@ public class RulesParser
 				// this is the POS tag of the token
 				String pos = token.get(PartOfSpeechAnnotation.class);     
 	      	}
-      	}		
+      	}*/
 
-	}*/
+      	ArrayList<Direction> motionTypes = new ArrayList<Direction>(1);
+
+      	CoreMap current = sentences.get(27); //TODO: automate the actual finding of the sentence!
+      	String dependenciesString = current.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class).toString(SemanticGraph.OutputFormat.LIST);
+      	String[] dependencies = dependenciesString.split("\n");
+		for (String s: dependencies)
+		{
+			if (s.contains("advmod(move")) //TODO: implement lemmatization - looking for the stem move, not the exact word "move"!
+			{
+				int startIndex = s.indexOf(' ') + 1;
+				int endIndex = s.indexOf('-', startIndex);
+
+				String adverb = s.substring(startIndex,endIndex);
+				switch (adverb) //TODO: synonyms of each adverb (use wordnet?)
+				{
+					case "diagonally":
+						motionTypes.add(Direction.DIAGONAL);
+						break;
+					case "forward":
+						motionTypes.add(Direction.FORWARD);
+						break;
+					//TODO: add the other ones (they don't matter right now lol);
+				}
+			}
+		}
+		return motionTypes;
+	}
 
 	public enum Direction
 	{

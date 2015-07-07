1d5a8b27d4715b4a6459a23bf30ccb70b6c71d68
Seth Benjamin <sethjbenjamin@gmail.com>
2015-07-06 22:25:12 -0500

    determineMotionSentences() no longer excludes sentences with negation words. parseMotion() handles them properly instead.

--- RulesParser.java
+++ RulesParser.java
@@ -502,8 +502,6 @@ public class RulesParser
 		  (the truth value of this property is stored by the boolean variable isMoveArgument)
 		- the sentence is not a transition sentence for p (that is, a sentence that describes how p can turn into a different
 		  type of piece) (this is determined by calling p.isTransitionSentence(i) where i is the index of the sentence)
-		- the sentence does not contain a negation word (ie, it contains no negative dependencies - the truth value of this property
-		  is stored by the boolean varaible isNegative)
 
 		There are a few exceptions:
 		- if p is a default type whose name is not "piece", "piece" can substitute for name in the previous properties and the sentence
@@ -523,10 +521,10 @@ public class RulesParser
 		//iterate over all sentences
 		for (int i = 0; i < sentences.size(); i++)
 		{
-			CoreMap currentSentence = sentences.get(i); //ith (current) sentence
+			CoreMap sentence = sentences.get(i); //ith (current) sentence
 
 			//dependencies of the current sentence
-			String[] currentDependencies = currentSentence.get(
+			String[] dependencies = sentence.get(
 				SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class).toString(
 				SemanticGraph.OutputFormat.LIST).split("\n");
 
@@ -535,25 +533,18 @@ public class RulesParser
 			String compoundedNoun = null;
 			int compoundedNounIndex = -1;
 			boolean isMovePredicate = false;
-			boolean isNegative = false;
 			int index = -1;
 
 			//iterate over all dependencies of the current sentence
-			for (int j = 1; j < currentDependencies.length; j++)
+			for (int j = 1; j < dependencies.length; j++)
 			{
-				String d = currentDependencies[j];
+				String d = dependencies[j];
 				int index1 = isolateIndexFromDependency(d,1);
 				int index2 = isolateIndexFromDependency(d,2);
 				String lemma1 = lemmas[i][index1-1];
 				String lemma2 = lemmas[i][index2-1];
 				String pos1 = partsOfSpeech[i][index1-1];
 				String pos2 = partsOfSpeech[i][index2-1];
-
-				/* The following if statement checks for negation words in the current sentence (by looking for negative dependencies).
-				The method currently ignores sentences that have negation words, such that a sentence 
-				like "Checkers cannot move backward." is not parsed. */
-				if (d.contains("neg("))
-					isNegative = true;
 
 				/* The following if statement checks if name is a modifier in a noun compound. This matters because sometimes a noun compound,
 				of which the current name of the piece is the modifying noun, is used instead of just name itself, for example:
@@ -648,9 +639,8 @@ public class RulesParser
 			- one of the allowed movetypes is a predicate, which takes either name or a pronoun referring to it as an argument 
 			  (that is, if isMovePredicate == true)
 			- the sentence is not a transition sentence for p
-			- the sentence is not negative (that is, if isNegative == false)
 			*/
-			if (isMovePredicate && !p.isTransitionSentence(i) && !isNegative)
+			if (isMovePredicate && !p.isTransitionSentence(i))
 			{
 				if (!indices.contains(index)) //also, we don't want to add multiple of the same index
 				{
@@ -671,6 +661,10 @@ public class RulesParser
 
 		for (int i: indices)
 		{
+			ArrayList<Integer> negatedWords = new ArrayList<Integer>(1);
+			/*this ArrayList will hold negated words in the current sentence (those modified by a negation word); 
+			if a direction word or the word it modifies is in this list, it will not be added to motionTypes */
+
 			CoreMap sentence = sentences.get(i); //the current sentence
 
 			//dependencies for current sentence as a String[], each entry containing a single dependency String
@@ -679,20 +673,25 @@ public class RulesParser
 				SemanticGraph.OutputFormat.LIST).split("\n");
 
 			//iterate over all dependencies, searching for certain types
-			for (String d: dependencies)
+			for (int j = 1; j < dependencies.length; j++)
 			{
-				if (d.contains("advmod") || d.contains("amod")) // check for adverbs modifying verbs, or adjectives modifying nouns
+				String d = dependencies[j];
+				int index1 = isolateIndexFromDependency(d, 1); 
+				int index2 = isolateIndexFromDependency(d, 2); 
+				String lemma1 = lemmas[i][index1-1]; 
+				String lemma2 = lemmas[i][index2-1]; 
+
+				if (d.contains("neg("))
+					negatedWords.add(index1);
+				else if (d.contains("dep(") && lemma1.equals("not"))
+					negatedWords.add(index2);
+
+				if (d.contains("advmod(") || d.contains("amod(")) // check for adverbs modifying verbs, or adjectives modifying nouns
 				{
-					/* We want to determine if the word being modified is any hyponym of the verb "move" being modified by a
-					directional adverb, or any synonym of the nouns "move" and "direction" being modified by a directional adjective. 
-					We first have to determine what the modified word's lemma is. 
-					To do this, we isolate the word's index in the sentence as a substring of the dependency string. */
-					int modifiedIndex = isolateIndexFromDependency(d, 1); //the word is the first word in the dependency substring
+					/* We want to determine if the word being modified is one of the allowed move types being modified by a
+					directional adverb, or any synonym of the nouns "move" and "direction" being modified by a directional adjective. */
 
-					//Now we determine the lemma of the modified word.
-					String modified = lemmas[i][modifiedIndex-1]; //-1 because the sentence indices start from 1, not 0
-
-					/*Now that we have the modified word's lemma, we determine if it is either:
+					/* The following determines if lemma1 (the modified word) is:
 					-one of the allowed move types
 					-a synonym of "move (n)" or "direction (n)"
 					-a coordinating conjunction (explained below)
@@ -708,39 +707,30 @@ public class RulesParser
 					produce the following bizarre constructions: "advmod(move, and)", "advmod(and, forward)", "advmod(and, backward)". 
 					Since our example sentence is one of the more common ways English language rulesets describe the motion of kings,
 					we must also check if modified is a coordinating conjunction. */
-					if (moveTypes.contains(modified) || isSynonymOf("move", modified) || isSynonymOf("direction", modified) || isCoordinatingConjunction(modified))
+					if (moveTypes.contains(lemma1) || isSynonymOf("move", lemma1) || isSynonymOf("direction", lemma1) || isCoordinatingConjunction(lemma1))
 					{
-						//We now have to isolate the modifier as a substring.
-						String modifier = isolateWordFromDependency(d,2); //the modifier is the second word in the dependency substring
-
-						/*Now, we call addDirection() to check if "modifier" is a directional adverb or adjective, and if so, to
-						add it to "motionTypes". */
-						addDirection(modifier, motionTypes, i, name); //TODO: remove i!!!
+						/*Now, we call addDirection() to check if lemma2, the modifier, is a directional adverb or adjective, and if so, to
+						add it to motionTypes. */
+						if (!negatedWords.contains(index1) && !negatedWords.contains(index2))
+							addDirection(lemma2, motionTypes, i, name); //TODO: remove i!!!
 					}
 				}
 				else if (d.contains("nmod:toward")) //check for a PP like "toward the opponent"
 				{
-					/*First we have to check if the word being modified by the PP is a hyponym of the verb "move," or a synonym of the nouns 
-					"move" or "direction."
-					Again, to determine what the modified word is, we have to isolate its index in the sentence as a substring of the 
-					dependency string. */
-					int modifiedIndex = isolateIndexFromDependency(d,1); //the modified word is the first word in the dependency substring
-
-					//Now we determine the lemma of the modified word, and see if it is one of the move types
-					String modified = lemmas[i][modifiedIndex-1]; //-1 because the sentence indices start from 1, not 0
-
-					if (moveTypes.contains(modified) || isSynonymOf("direction", modified) || isSynonymOf("move", modified))
+					/* The following checks if the word being modified by the PP is one of the allowed move types or a synonym of the nouns 
+					"move" or "direction." */
+					if (moveTypes.contains(lemma1) || isSynonymOf("direction", lemma1) || isSynonymOf("move", lemma1))
 					{
 						/*At this point, we have determined that the phrase we are looking at is a PP of the form "toward/towards [DP]" 
-						modifying some hyponym of "move." Now we have to determine the object of the proposition, and what direction of motion
-						this object implies. 
+						modifying a motion predicate, or either of the nouns "move" or "direction". 
+						Now we have to determine the object of the proposition, and what direction of motion this object implies. 
 						For now, since it is the only such phrase that occurs in our 10 sample rulesets, the only object of "toward"/"towards"
 						that we will consider is the noun "opponent." This entails forward motion: "checkers can only move toward the opponent" 
 						is equivalent to "checkers can only move forward". */
 						String noun = isolateWordFromDependency(d,2); //the verb is the second word in the dependency substring
 						if (isSynonymOf("opponent", noun))
 						{
-							if (motionTypes.indexOf(Direction.FORWARD) < 0)
+							if (motionTypes.indexOf(Direction.FORWARD) < 0) //TODO: maybe add a negation check
 								motionTypes.add(Direction.FORWARD);
 							System.out.println("Sentence " + i + ": Forward motion added for " + name + " as a modifying PP"); //debugging
 
@@ -750,6 +740,58 @@ public class RulesParser
 			}
 		}
 		return motionTypes;
+	}
+
+	/**
+	Used by parseMotion() to update an ArrayList containing the allowed types of motion for a piece.
+	Given a reference to a String "word" and an ArrayList<Direction> "motionTypes" holding certain Directions (representing the allowed
+	types of motion for a piece), this method does the following:
+	-Checks if "word" is a synonym of any of the directional words "diagonal"/"diagonally", "forward," "backward," "left," and "right." 
+	  -If "word" is not a synonym of any of these directional words, the method does nothing else.
+	  -If it is, the method then checks if whatever direction "word" entails has been added to "motionTypes" yet. 
+	    -If it has not yet been added, the method adds that direction to "motionTypes."
+	    -If it has already been added, the method does nothing else.
+	*/
+	public void addDirection(String word, ArrayList<Direction> motionTypes, int i, String name) 
+	{
+		/*ALL of the following specifications of indices (used when calling isSynonymOf()) are specific to the WordNet 3.0 database! 
+		They must be changed for future versions of WordNet, as the indices of definitions change. */
+
+		 //5,6 are the indices in Wordnet 3.0 of the definitions of "diagonal" that denote direction
+		if (isSynonymOf("diagonal", word, 5, 6) || isSynonymOf("diagonally", word))
+		{
+			if (motionTypes.indexOf(Direction.DIAGONAL) < 0) //check to see if this type of motion has already been parsed
+				motionTypes.add(Direction.DIAGONAL);
+			System.out.println("Sentence " + i + ": Diagonal motion added for " + name); //debugging
+		}
+		//3,6,7,9,11 are the indices in Wordnet 3.0 of the definitions of "forward" that denote direction
+		else if (isSynonymOf("forward", word, 3, 6, 7, 9, 11))
+		{
+			if (motionTypes.indexOf(Direction.FORWARD) < 0) 
+				motionTypes.add(Direction.FORWARD);
+			System.out.println("Sentence " + i + ": Forward motion added for " + name); //debugging
+		}
+		//0,2,3 are the indices in Wordnet 3.0 of the definitions of "backward" that denote direction
+		else if (isSynonymOf("backward", word, 0, 2, 3))
+		{
+			if (motionTypes.indexOf(Direction.BACKWARD) < 0)
+				motionTypes.add(Direction.BACKWARD);
+			System.out.println("Sentence " + i + ": Backward motion added for " + name); //debugging
+		}
+		//19 is the index in Wordnet 3.0 of the definitions of "left" that denote direction
+		else if (isSynonymOf("left", word, 19))
+		{
+			if (motionTypes.indexOf(Direction.LEFT) < 0)
+				motionTypes.add(Direction.LEFT);
+			System.out.println("Sentence " + i + ": Leftward motion added for " + name); //debugging
+		}
+		//12,20 are the indices in Wordnet 3.0 of the definitions of "right" that denote direction
+		else if (isSynonymOf("right", word, 12, 20))
+		{
+			if (motionTypes.indexOf(Direction.RIGHT) < 0)
+				motionTypes.add(Direction.RIGHT);
+			System.out.println("Sentence " + i + ": Rightward motion added."); //debugging
+		}
 	}
 
 	/**
@@ -964,59 +1006,6 @@ public class RulesParser
 	{
 		return word.equals("for") || word.equals("and") || word.equals("nor") || 
 		word.equals("but") || word.equals("or") || word.equals("yet") || word.equals("so");
-	}
-
-	/**
-	Used by parseMotion() to update an ArrayList containing the allowed types of motion for a piece.
-	Given a reference to a String "word" and an ArrayList<Direction> "motionTypes" holding certain Directions (representing the allowed
-	types of motion for a piece), this method does the following:
-	-Checks if "word" is a synonym of any of the directional words "diagonal"/"diagonally", "forward," "backward," "left," and "right." 
-	  -If "word" is not a synonym of any of these directional words, the method does nothing else.
-	  -If it is, the method then checks if whatever direction "word" entails has been added to "motionTypes" yet. 
-	    -If it has not yet been added, the method adds that direction to "motionTypes."
-	    -If it has already been added, the method does nothing else.
-	*/
-	public void addDirection(String word, ArrayList<Direction> motionTypes, int i, String name) 
-	{
-		/*ALL of the following specifications of indices (used when calling isSynonymOf()) are specific to the WordNet 3.0 database! 
-		They must be changed for future versions of WordNet, as the indices of definitions change. */
-
-		 //5,6 are the indices in Wordnet 3.0 of the definitions of "diagonal" that denote direction
-		if (isSynonymOf("diagonal", word, 5, 6) || isSynonymOf("diagonally", word))
-		{
-			if (motionTypes.indexOf(Direction.DIAGONAL) < 0) //check to see if this type of motion has already been parsed
-				motionTypes.add(Direction.DIAGONAL);
-			System.out.println("Sentence " + i + ": Diagonal motion added for " + name); //debugging
-		}
-		//3,6,7,9,11 are the indices in Wordnet 3.0 of the definitions of "forward" that denote direction
-		else if (isSynonymOf("forward", word, 3, 6, 7, 9, 11))
-		{
-			if (motionTypes.indexOf(Direction.FORWARD) < 0) 
-				motionTypes.add(Direction.FORWARD);
-			System.out.println("Sentence " + i + ": Forward motion added for " + name); //debugging
-		}
-		//0,2,3 are the indices in Wordnet 3.0 of the definitions of "backward" that denote direction
-		else if (isSynonymOf("backward", word, 0, 2, 3))
-		{
-			if (motionTypes.indexOf(Direction.BACKWARD) < 0)
-				motionTypes.add(Direction.BACKWARD);
-			System.out.println("Sentence " + i + ": Backward motion added for " + name); //debugging
-		}
-		//19 is the index in Wordnet 3.0 of the definitions of "left" that denote direction
-		else if (isSynonymOf("left", word, 19))
-		{
-			if (motionTypes.indexOf(Direction.LEFT) < 0)
-				motionTypes.add(Direction.LEFT);
-			System.out.println("Sentence " + i + ": Leftward motion added for " + name); //debugging
-		}
-		//12,20 are the indices in Wordnet 3.0 of the definitions of "right" that denote direction
-		else if (isSynonymOf("right", word, 12, 20))
-		{
-			if (motionTypes.indexOf(Direction.RIGHT) < 0)
-				motionTypes.add(Direction.RIGHT);
-			System.out.println("Sentence " + i + ": Rightward motion added."); //debugging
-		}
-
 	}
 	
 }
